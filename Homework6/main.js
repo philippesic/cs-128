53ff8a527c3e69fa69cb9818e65841ec61cc2912
Pippo Pesic <philippesic06@gmail.com>
2023-05-07 12:26:51 -0700

    Added sw registration check and updated large screen content alignment

--- Homework6/main.js
+++ Homework6/main.js
@@ -1,15 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-          navigator.serviceWorker.register('./service-worker.js')
-            .then(registration => {
-              console.log('Service worker registered:', registration);
-            })
-            .catch(error => {
-              console.log('Service worker registration failed:', error);
-            });
+          navigator.serviceWorker.getRegistrations().then(registrations => {
+            if (registrations.length === 0) {
+              navigator.serviceWorker.register('./service-worker.js')
+                .then(registration => {
+                  console.log('Service worker registered:', registration);
+                })
+                .catch(error => {
+                  console.log('Service worker registration failed:', error);
+                });
+            }
+          });
         });
-      }
+      }      
   });
   
